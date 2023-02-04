c79226edd335e8c6e9aa94b204c05e17f69d67fe
Marton Sari <sarimarton@gmail.com>
2023-02-04 21:53:11 +0100

    config update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tilajs', // Usually your GitHub org/user name.
-  projectName: 'tila', // Usually your repo name.
+  projectName: '', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
